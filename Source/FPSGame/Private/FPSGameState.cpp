690211a9b833bf2f4eab4160b2678f29ffb480a9
trurtrl <trurtrl@gmail.com>
2018-07-12 11:38:13 +0300

    Game State part 3. BP widget "Mission Complete" is showed on Server and Clients (before only on Server) - realization appearing the widget moved frrom BP Game Mode to BP Player Controller

--- Source/FPSGame/Private/FPSGameState.cpp
+++ Source/FPSGame/Private/FPSGameState.cpp
@@ -1,20 +1,37 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSGameState.h"
-
+#include "FPSPlayerController.h"
 
 
 void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSuccess)
 {
 	//	get all pawns that exist on the level
-	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++)
-	{
-		APawn* Pawn = It->Get();
-
+//	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++)
+//	{
+//		APawn* Pawn = It->Get();
+//
 		//	and ...
-		if (Pawn && Pawn->IsLocallyControlled())
+//		if (Pawn && Pawn->IsLocallyControlled())
+//		{
+//			Pawn->DisableInput(nullptr);
+//		}
+//	}
+
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
+	{
+		AFPSPlayerController* PC = Cast<AFPSPlayerController>(It->Get());
+		if (PC && PC->IsLocalController())
 		{
-			Pawn->DisableInput(nullptr);
+			PC->OnMissionComplete(InstigatorPawn, bMissionSuccess);
+
+			//	Replaces previous loop "for", that disables input
+			APawn* Pawn = PC->GetPawn();
+			if (Pawn)
+			{
+				Pawn->DisableInput(PC);
+			}	
+
 		}
 	}
 }
